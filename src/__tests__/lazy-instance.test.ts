20d65c817f29b4b4ac2290b8c3b4dbdabbafd2a4
Tiago da Costa Peixoto <tiagocpeixoto@gmail.com>
2021-12-28 09:27:46 -0300

    add prettier endOfLine auto config

--- src/__tests__/lazy-instance.test.ts
+++ src/__tests__/lazy-instance.test.ts
@@ -1,55 +1,124 @@
 import * as faker from "faker";
-import { LazyInstance } from "..";
+import { AsyncLazyInstance, LazyInstance } from "..";
 
 describe("lazy-instance tests", function () {
-  it("test default create and get lazy-instance", function () {
-    let value = "";
+  describe("LazyInstance tests", function () {
+    it("test default create and get", function () {
+      let value = "";
 
-    const lazyInstance = new LazyInstance(() => (value = faker.lorem.word()));
+      const lazyInstance = new LazyInstance(() => (value = faker.lorem.word()));
 
-    expect(value).toBeFalsy();
-    const getValue = lazyInstance.get;
-    expect(value).toBe(getValue);
-  });
-
-  it("test eager init", function () {
-    let value = "";
-
-    const lazyInstance = new LazyInstance(() => (value = faker.lorem.word()), {
-      eagerInit: true,
+      expect(value).toBeFalsy();
+      const getValue = lazyInstance.get;
+      expect(value).toBe(getValue);
     });
 
-    expect(value).toBeTruthy();
-    const getValue = lazyInstance.get;
-    expect(value).toBe(getValue);
-  });
+    it("test eager init", function () {
+      let value = "";
 
-  it("test named create and get from lazy-instance", function () {
-    const name = faker.lorem.word();
-    let value = "";
+      const lazyInstance = new LazyInstance(
+        () => (value = faker.lorem.word()),
+        {
+          eagerInit: true,
+        }
+      );
 
-    const lazyInstance = new LazyInstance(() => (value = faker.lorem.word()), {
-      name,
+      expect(value).toBeTruthy();
+      const getValue = lazyInstance.get;
+      expect(value).toBe(getValue);
     });
-    expect(value).toBeFalsy();
 
-    const getValue = lazyInstance.get;
-    expect(value).toBe(getValue);
+    it("test named create and get", function () {
+      const name = faker.lorem.word();
+      let value = "";
 
-    expect(lazyInstance.instanceName).toMatch(name);
+      const lazyInstance = new LazyInstance(
+        () => (value = faker.lorem.word()),
+        {
+          name,
+        }
+      );
+      expect(value).toBeFalsy();
+
+      const getValue = lazyInstance.get;
+      expect(value).toBe(getValue);
+
+      expect(lazyInstance.instanceName).toMatch(name);
+    });
+
+    it("test reset", function () {
+      let counter = 0;
+      const lazyInstance = new LazyInstance(() => ++counter);
+      expect(lazyInstance.get).toBe(1);
+      expect(counter).toBe(1);
+      // again
+      expect(lazyInstance.get).toBe(1);
+      expect(counter).toBe(1);
+      // reset
+      lazyInstance.reset();
+      expect(lazyInstance.get).toBe(2);
+      expect(counter).toBe(2);
+    });
   });
 
-  it("test reset", function () {
-    let counter = 0;
-    const lazyInstance = new LazyInstance(() => ++counter);
-    expect(lazyInstance.get).toBe(1);
-    expect(counter).toBe(1);
-    // again
-    expect(lazyInstance.get).toBe(1);
-    expect(counter).toBe(1);
-    // reset
-    lazyInstance.reset();
-    expect(lazyInstance.get).toBe(2);
-    expect(counter).toBe(2);
+  describe("AsyncLazyInstance tests", function () {
+    it("test default create and get", async function () {
+      let value = "";
+
+      const asyncLazyInstance = new AsyncLazyInstance(
+        async () => (value = faker.lorem.word())
+      );
+
+      expect(value).toBeFalsy();
+      const getValue = await asyncLazyInstance.get;
+      expect(value).toBe(getValue);
+    });
+
+    it("test eager init", async function () {
+      let value = "";
+
+      const asyncLazyInstance = new AsyncLazyInstance(
+        async () => (value = faker.lorem.word()),
+        {
+          eagerInit: true,
+        }
+      );
+
+      expect(value).toBeTruthy();
+      const getValue = await asyncLazyInstance.get;
+      expect(value).toBe(getValue);
+    });
+
+    it("test named create and get", async function () {
+      const name = faker.lorem.word();
+      let value = "";
+
+      const asyncLazyInstance = new AsyncLazyInstance(
+        async () => (value = faker.lorem.word()),
+        {
+          name,
+        }
+      );
+      expect(value).toBeFalsy();
+
+      const getValue = await asyncLazyInstance.get;
+      expect(value).toBe(getValue);
+
+      expect(asyncLazyInstance.instanceName).toMatch(name);
+    });
+
+    it("test reset", async function () {
+      let counter = 0;
+      const asyncLazyInstance = new AsyncLazyInstance(async () => ++counter);
+      expect(await asyncLazyInstance.get).toBe(1);
+      expect(counter).toBe(1);
+      // again
+      expect(await asyncLazyInstance.get).toBe(1);
+      expect(counter).toBe(1);
+      // reset
+      asyncLazyInstance.reset();
+      expect(await asyncLazyInstance.get).toBe(2);
+      expect(counter).toBe(2);
+    });
   });
 });
--- src/lazy-instance.ts
+++ src/lazy-instance.ts
@@ -2,26 +2,36 @@ export interface InitFunction<T> {
   (): T;
 }
 
+export interface AsyncInitFunction<T> {
+  (): Promise<T>;
+}
+
 export interface InitOptions {
   name?: string;
   eagerInit?: boolean;
 }
 
 export class LazyInstance<T> {
-  private instance?: T;
-  private readonly name?: string;
-  private readonly lazyInit: InitFunction<T>;
+  readonly name: string;
+  readonly eagerInit: boolean;
+  #instance?: T;
+  readonly #lazyInit: InitFunction<T>;
 
   constructor(
     lazyInit: InitFunction<T>,
     { name, eagerInit = false }: InitOptions = {}
   ) {
-    this.lazyInit = lazyInit;
-    this.name = `${name || "LazyInstance"}-${new Date().toISOString()}`;
+    this.name = `${name ?? "LazyInstance"}-${new Date().toISOString()}`;
+    this.eagerInit = eagerInit;
+    this.#lazyInit = lazyInit;
 
-    if (eagerInit) {
-      this.instance = this.lazyInit();
+    if (this.eagerInit) {
+      this.init();
     }
+  }
+
+  init(): void {
+    this.#instance = this.#lazyInit();
   }
 
   get instanceName(): string {
@@ -33,10 +43,52 @@ export class LazyInstance<T> {
     //     console.debug(`Lazying init ${this.name}...`)
     // }
 
-    return this.instance || (this.instance = this.lazyInit());
+    return this.#instance ?? (this.#instance = this.#lazyInit());
   }
 
   reset(): void {
-    this.instance = undefined;
+    this.#instance = undefined;
+  }
+}
+
+export class AsyncLazyInstance<T> {
+  readonly name: string;
+  readonly eagerInit: boolean;
+  #instance?: T;
+  readonly #asyncLazyInit: AsyncInitFunction<T>;
+
+  constructor(
+    asyncLazyInit: AsyncInitFunction<T>,
+    { name, eagerInit = false }: InitOptions = {}
+  ) {
+    this.name = `${name ?? "AsyncLazyInstance"}-${new Date().toISOString()}`;
+    this.eagerInit = eagerInit;
+    this.#asyncLazyInit = asyncLazyInit;
+
+    if (this.eagerInit) {
+      this.asyncInit().catch(console.error);
+    }
+  }
+
+  async asyncInit(): Promise<void> {
+    this.#instance = await this.#asyncLazyInit();
+  }
+
+  get instanceName(): string {
+    return this.name as string;
+  }
+
+  get get(): Promise<T> {
+    /**
+     * It's an IIFE
+     * @see https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+     */
+    return (async () => {
+      return this.#instance ?? (this.#instance = await this.#asyncLazyInit());
+    })();
+  }
+
+  reset(): void {
+    this.#instance = undefined;
   }
 }
